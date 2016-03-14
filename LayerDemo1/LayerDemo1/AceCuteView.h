3f10c153b777d8a1eddd97ee0fc222a484e31cd5
AceWei <446839735@qq.com>
2016-03-14 13:04:33 +0800

    修改补丁

--- LayerDemo1/LayerDemo1/AceCuteView.h
+++ LayerDemo1/LayerDemo1/AceCuteView.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "ConfigHeader.h"
 
 @interface AceCuteView : UIView
 
--- LayerDemo1/LayerDemo1/SecondViewController.m
+++ LayerDemo1/LayerDemo1/SecondViewController.m
@@ -20,7 +20,12 @@
     // Do any additional setup after loading the view.
     
     AceCuteView *view = [[AceCuteView alloc] initWithFrame:CGRectMake(50, 50, 40, 25)];
+    //黏性距离，不设置默认50，允许设置范围30~90
     view.viscosity = 90;
+    //需要显示的文字
+    view.bubbleText = @"55";
+    //小圆点背景色，默认是红色
+    view.bgColor = [UIColor redColor];
     [self.view addSubview:view];
     
     
